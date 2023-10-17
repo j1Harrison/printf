6d0a74ea80b7c641fa1ea1cb29feb6f4a5b4533e
j1Harrison <mfonobong625@gmail.com>
2023-10-17 04:06:32 -0700

    print_f.c

--- print_f.c
+++ print_f.c
@@ -1,12 +1,13 @@
 #include "main.h"
+#include <unistd.h>
 
 int _printf(const char *format, ...)
 {
 	int chara_print = 0;
 	va_list list_of_args;
 
-	if (format ==  NULL)
-		return (-1);
+	if (format == NULL)
+	return (-1);
 
 	va_start(list_of_args, format);
 
@@ -20,34 +21,37 @@ int _printf(const char *format, ...)
 		else
 		{
 			format++;
-			if (*format == '\0')
-				break;
+		if (*format == '\0')
+		break;
 
-			if (*format == '%')
-			{
-				write(1, format, 1);
-				chara_print++;
-			}
-			else if (*format == 'c')
-			{
-				char c = va_arg(list_of_args, int);
-
-				write(1, &c, 1);
-				chara_print++;
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(list_of_args, char*);
-				int str_len = 0;
-
-				while (str[str_len] != '0')
-					str_len++;
-
-				write(1, str, str_len);
-				chara_print += str_len;
-			}
-			format++;
+		if (*format == '%')
+		{
+			write(1, format, 1);
+			chara_print++;
 		}
-		va_end(list_of_args);
-		return (chara_print);
+		else if (*format == 'c')
+		{
+			char c = va_arg(list_of_args, int);
+
+			write(1, &c, 1);
+			chara_print++;
+		}
+		else if (*format == 's')
+		{
+			char *str = va_arg(list_of_args, char*);
+			int str_len = 0;
+
+		while (str[str_len] != '\0')
+		str_len++;
+
+			write(1, str, str_len);
+			chara_print += str_len;
+		}
+		format++;
+		}
+	}
+
+	va_end(list_of_args);
+	return (chara_print);
 }
+
