1fe2d32b38760a58d1c7cfe9d623ef94086a7e15
j1Harrison <mfonobong625@gmail.com>
2023-10-17 04:11:50 -0700

    print_f.c

--- main.c
+++ main.c
@@ -20,7 +20,7 @@ int main(void)
 	addr = (void *)0x7ffe637541f0;
 	_printf("Length:[%d, %i]\n", len, len);
 	printf("Length:[%d, %i]\n", len2, len2);
- 	_printf("Negative:[%d]\n", -762534);
+	_printf("Negative:[%d]\n", -762534);
 	printf("Negative:[%d]\n", -762534);
 	_printf("Unsigned:[%u]\n", ui);
 	printf("Unsigned:[%u]\n", ui);
